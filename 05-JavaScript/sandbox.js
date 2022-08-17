2574fbc73447da63981fc10a5300a740c5f82ac1
YuanLinTech <66548341+YuanLinTech@users.noreply.github.com>
2022-08-17 19:49:01 +1000

    committed

--- 05-JavaScript/sandbox.js
+++ 05-JavaScript/sandbox.js
@@ -4,4 +4,32 @@ var student;
 student = 'Jing';
 student = 'Emily';
 
-console.log(student);
+var student1, student2, student3;
+
+student1 = 'Jim';
+student2 = 'Tom';
+student3 = 'Jack';
+
+// console.log(student1);
+// console.log(student2);
+// console.log(student3);
+
+var a = 0;
+a = 'a string value';
+a = true;
+// console.log(a);
+
+// hoisting
+var car;
+car = 'Honda civic'
+console.log(car);
+
+var aNum = 3.1415926;
+aNum = NaN; // Not a number
+
+if (isNaN(aNum))
+{
+    console.log('It\'s not a Number.')
+}
+
+// The string data type can be used to express both character and strins
