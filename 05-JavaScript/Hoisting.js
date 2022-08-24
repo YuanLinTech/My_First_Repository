3105e81c35c16a148bb85db195bab411dde6ab53
Yuan Lin <yu.an.lintech@gmail.com>
2022-08-25 00:40:34 +1000

    committed

--- 05-JavaScript/Hoisting.js
+++ 05-JavaScript/Hoisting.js
@@ -7,3 +7,15 @@ CarName = "Saab";
 // let carName; ReferenceError: Cannot access 'CarName' before initialization
 var CarName;
 console.log(CarName);
+
+// var x = 5; // Initialize x
+// var y = 7; // Initialize y
+// console.log(x + " " + y); Output: 5 7
+
+var x = 5; // Initialize x
+console.log(x + " " + y); // Output: 5 undefined
+var y = 7; // Initialize y
+
+z = 5; // Initialize x
+console.log(z); // Output: 5 undefined
+let z = 7; // Initialize y
