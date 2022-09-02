2f904f48d57bbf5f4173786d3428fb3358198258
Yuan Lin <yu.an.lintech@gmail.com>
2022-09-02 14:23:44 +1000

    committed

--- 14-Java/Welcome.java
+++ 14-Java/Welcome.java
@@ -1,5 +1,8 @@
-public class Welcome {
-  public static void main(String[] args) {
-// Display message Welcome to Java! on the console
-System.out.println("Welcome to Java!"); }
+public class Welcome 
+{
+  public static void main(String[] args) 
+  {
+    // Display message Welcome to Java! on the console
+    System.out.println("Welcome to Java!"); 
+  }
 }
