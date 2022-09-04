ed8a209ffb7fd65fa5256d3836f2ae5e4f26290c
Yuan Lin <yu.an.lintech@gmail.com>
2022-09-04 21:28:03 +1000

    committed

---
+++ 05-JavaScript/Flight_Stops.js
@@ -0,0 +1,16 @@
+function getStops(flights)
+{
+    let stop = flights.length - 1;
+    if (stop == 0)
+    {
+        return 'Direct';
+    }
+    else if (stop == 1)
+    {
+        return '1 stop';
+    }
+    else 
+    {
+        return stop + ' stops';
+    }
+}
