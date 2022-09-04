74a1874ddf418c93972f66db0013cfb70f7aed69
Yuan Lin <yu.an.lintech@gmail.com>
2022-09-05 01:39:04 +1000

    committed

--- 05-JavaScript/truthy_and_falsy.js
+++ 05-JavaScript/truthy_and_falsy.js
@@ -3,44 +3,49 @@
 // falsy && something === false
 
 var a1 = true && "dog";
-console.log(a1);
+console.log(a1); // Output: "dog"
 
 var a2 = "cat" && "dog";
-console.log(a2);
+console.log(a2); // Output: "dog"
 
 var a3 = false && "cat";
-console.log(a3);
+console.log(a3); // Output: false
 
 var a4 = "cat" && false;
-console.log(a4);
+console.log(a4); // Output: false
 
-var a5 = "" && false;
-console.log(a5);
+var a5 = "" && "cat";
+console.log(a5); // Output: false
 
 var a6 = false && "";
-console.log(a6);
+console.log(a6); // Output: false
+
+var a7 = "" && false;
+console.log(a7); // Output: false
 
 // truthy || something === true
 // falsy || something === something
 
 var b1 = true || "dog";
-console.log(b1);
+console.log(b1); // Output: true
 
 var b2 = "cat" || "dog";
-console.log(b2);
+console.log(b2); // Output: true
 
 var b3 = false || "cat";
-console.log(b3);
+console.log(b3); // Output: "cat"
 
 var b4 = "cat" || false;
-console.log(b4);
+console.log(b4); // Output: true
 
-var b5 = "" || false;
-console.log(b5);
+var b5 = "" || "cat";
+console.log(b5); // Output: "cat"
 
 var b6 = false || "";
-console.log(b6);
+console.log(b6); // Output: ""
 
+var b7 = "" && false;
+console.log(b7); // Output: false
 
 /* var c = a && b
 If a is truthy, then c === b.
