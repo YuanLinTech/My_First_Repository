df282e366bde0897a4ab90c66b8969ced9c0e135
Yuan Lin <yu.an.lintech@gmail.com>
2022-09-12 22:25:58 +1000

    committed

--- 05-JavaScript/Tax_rate.js
+++ 05-JavaScript/Tax_rate.js
@@ -39,7 +39,7 @@ function findTax(salary)
     {
         if (salary > salaryRange[i])
         {
-            const tax = (salary - salaryRange[i]) * taxRate[i] + starNum[i];
+            const tax = (salary - salaryRange[i]) * taxRate[i] + startNum[i];
             return tax;
         }
     }
@@ -49,11 +49,11 @@ console.log(findTax(180000));
 
 var TAX_TABLE_2022 = 
 [
-    { min: 0, max: 18200, tax: 0, rate: 0},
-    { min: 18200, max: 45000, tax: 0, rate: 0.19 },
-    { min: 45000, max: 120000, tax: 5092, rate: 0.325 },
-    { min: 120000, max: 180000, tax: 29467, rate: 0.37 },
-    { min: 180000, max: Number.POSITIVE_INFINITY, tax: 51667, rate: 0.45 },
+    { min: 0,      max: 18200,  tax: 0,     rate: 0},
+    { min: 18200,  max: 45000,  tax: 0,     rate: 0.19},
+    { min: 45000,  max: 120000, tax: 5092,  rate: 0.325},
+    { min: 120000, max: 180000, tax: 29467, rate: 0.37},
+    { min: 180000, max: Number.POSITIVE_INFINITY, tax: 51667, rate: 0.45},
 ]
 
 function calculateTax(income, taxTable)
@@ -67,6 +67,8 @@ function calculateTax(income, taxTable)
         }
     }
 }
+
+console.log(160000, TAX_TABLE_2022);
 
 /*
 SOLID: The First 5 Principles of Object Oriented Design
