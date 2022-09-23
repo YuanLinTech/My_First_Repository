b1d7ce3d5e9784fb0de0930b8a1e83fd066782d1
Yuan Lin <yu.an.lintech@gmail.com>
2022-09-23 19:54:14 +1000

    committed

--- 05-JavaScript/Destructuring_assignment.js
+++ 05-JavaScript/Destructuring_assignment.js
@@ -1,13 +1,21 @@
-const x = [1, 2, 3, 4, 5];
-const [y, z, ...rest] = x;
+var x = [1, 2, 3, 4, 5];
+var [y, z, ...rest] = x;
 console.log(y); // y = 1
 console.log(z); // z = 2
 console.log(rest); // rest = [3, 4, 5]
 
-const obj = { a: 1, b: 2 };
-const { a, b } = obj;
+var obj = { a: 1, b: 2 };
+var { a, b } = obj;
 // is equivalent to:
-// const a = obj.a;
-// const b = obj.b;
+// var a = obj.a;
+// var b = obj.b;
 console.log(a); // a = 1
 console.log(b); // b = 2
+
+var x = ([a, b] = [10, 20]) => a + b; /* Used destructuring assignment to assign a the default value of 10 and b the default value of 20. */
+console.log(x()); // Output: 30
+console.log(x(2,3)); // Output: 5
+
+var y = ({ a, b } = { a: 10, b: 20 }) => a + b; /* Used destructuring assignment to assign a the default value of 10 and b the default value of 20. */
+console.log(y()); // Output: 30
+console.log(y(2,3)); // Output: 5
