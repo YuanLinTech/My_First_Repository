c4c1767ce761821708b56ff401ed8a595a836f69
Yuan Lin <yu.an.lintech@gmail.com>
2022-09-24 23:10:12 +1000

    committed

--- 05-JavaScript/Destructuring_assignments.js
+++ 05-JavaScript/Destructuring_assignments.js
@@ -11,6 +11,9 @@ var { a, b } = obj;
 // var b = obj.b;
 console.log(a); // a = 1
 console.log(b); // b = 2
+var { c, d } = obj;
+console.log(c); // Output: undefined
+console.log(d); // Output: undefined
 
 var x = ([a, b] = [10, 20]) => a + b; /* Used destructuring assignment to assign a the default value of 10 and b the default value of 20. */
 console.log(x()); // Output: 30
@@ -26,7 +29,7 @@ const numbers1 = [];
 const obj1 = { a: 1, b: 2 };
 // { a: numbers1[0], b: numbers1[1] } = obj1; // SyntaxError: Unexpected token ':'
 ({a: numbers1[0], b: numbers1[1]} = obj1); 
-// is equivalent to:
+// This is equivalent to:
 // numbers1[0] = obj1.a;
 // numbers1[1] = obj1.b;
 console.log(numbers1); // Output: [ 1, 2 ]
@@ -43,14 +46,60 @@ var obj2 = { a: 1, b: 2 };
 // numbers2[1] = obj2.b;
 console.log(numbers2); // Output: [ 1, 2 ]
 
-
 /* var { a: numbers2[0], b: numbers2[1] } = obj2; // SyntaxError: Invalid destructuring assignment target
 // This is equivalent to:
 // var numbers2[0] = obj2.a;
 // var numbers2[1] = obj2.b;
 // Which definitely is not valid. */
 
+var { a, ...others } = { a: 1, b: 2, c: 3 };
+console.log(a); 1
+console.log(others); // Output: { b: 2, c: 3 }
 
+const [first, ...others2] = [1, 2, 3];
+console.log(others2); // Output: [2, 3]
 
+var foo = ['one', 'two', 'three'];
+var [red, yellow, green] = foo;
+console.log(red); // Output: "one"
+console.log(yellow); // Output: "two"
+console.log(green); // Output: "three"
 
+var foo = ['one', 'two'];
+var [red, yellow, green, blue] = foo;
+console.log(red); // Output: "one"
+console.log(yellow); // Output: "two"
+console.log(green); // Output: undefined
+console.log(blue);  // Output: undefined
 
+// Swapping variables
+let m = 1;
+let n = 3;
+
+[m, n] = [n, m];
+console.log(m); // Output: 3
+console.log(n); // Output: 1
+
+const arr = [1, 2, 3];
+[arr[2], arr[1]] = [arr[1], arr[2]];
+console.log(arr); // Output: [1, 3, 2]
+
+function f() {
+    return [1, 2, 3];
+}
+
+[a, , b] = f();
+console.log(a); // Output: 1
+console.log(b); // Output: 3
+
+[c] = f();
+console.log(c); // Output: 1
+
+var o = { p: 42, q: true };
+var { p: foo, q: bar } = o;
+// This is equivalent to
+// const foo = o.p;
+// const bar = o.q;
+
+console.log(foo); // 42
+console.log(bar); // true
