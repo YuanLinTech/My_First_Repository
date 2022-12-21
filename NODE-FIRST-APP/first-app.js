08cfab21c1a7456ee692dacd2f7021fdf44f8bbb
Joseph Lin <yu.an.lintech@gmail.com>
2022-12-21 23:22:28 +1100

    committed

--- NODE-FIRST-APP/first-app.js
+++ NODE-FIRST-APP/first-app.js
@@ -1,5 +1,5 @@
-const fs = require('fs'); // Store the imported file system in a simple constant.
-fs.writeFileSync('Hello.txt','Hello from Node.js'); // The writeFileSync() method will write the file to our hard drive.
+import { writeFileSync } from 'fs'; // Store the imported file system in a simple constant.
+writeFileSync('Hello.txt','Hello from Node.js'); // The writeFileSync() method will write the file to our hard drive.
 /* And the argument it wants is the path to the file, including the file name.
 
 And here we could name this hello.txt.
