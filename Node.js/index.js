b290fbd8e3bcf73437e2012aec9e8cd1010f70fa
Joseph Lin <yu.an.lintech@gmail.com>
2022-12-21 21:50:12 +1100

    committed

--- Node.js/index.js
+++ Node.js/index.js
@@ -10,11 +10,11 @@ const object1 = {
 
 console.log(object1.cb());
 /*
-Hello, undefined!
-undefined
+Hello, undefined! (the this parameter for the foo() function is undefined)
+undefined (the foo() function returns undefined)
 */
 
-const object2 ={
+const object2 = {
     who: 'Mason',
     cb() {
         console.log(`Hello, ${this.who}!`);
@@ -28,11 +28,12 @@ function foo(cb){
 console.log(object2.cb());
 /*
 Hello, Mason!
-undefined
+undefined (the object2.cb() function returns undefined)
 */
-
+console.log(object2.cb); // [Function: cb]
 console.log(foo(object2.cb));
+// Note that console.log(foo(object2.cb())) will cause a TypeError: cb is not a function
 /*
-Hello, undefined!
-undefined
+Hello, undefined! (objects.cb is a function that returns undefined, so the argument for the foo() function is undefined)
+undefined (the foo() function returns undefined)
 */
