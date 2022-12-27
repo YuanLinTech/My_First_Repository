d70e8e53e657213d7f76ae7d58577d12c78ad050
Joseph Lin <yu.an.lintech@gmail.com>
2022-12-27 23:36:34 +1100

    added process.exit()

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -2,8 +2,10 @@ const http = require('http'); // Importing a global module named HTTP
 
 /* The first argument of request listener contains data about the request and the second one will help you send a response. 
 Node will execute the function whenever a request reaches our server. */
-http.createServer((req, res) => {
-    console.log(res);
+const server = http.createServer((req, res) => {
+    console.log(req); // Print server request
+    console.log(res); // Print server response
+    process.exit(); // Stop listening, unregister the server and quit the process
 }); 
 
 /*
@@ -16,3 +18,9 @@ is equivalent to
 http.createServer(function(req,res){
 
 }); */
+
+/* The listen() method actually starts a process where node.js will not immediately exit our script,
+ but where node.js will instead keep this running to listen, that's why the method is named like this for incoming requests.*/
+
+server.listen(3000); // 3000 is the port number
+
