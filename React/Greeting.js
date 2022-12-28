ffc5fd1daf7b456b485de402c3ec0a7319ab11b7
Joseph Lin <yu.an.lintech@gmail.com>
2022-12-28 23:59:40 +1100

    committed

---
+++ React/Greeting.js
@@ -0,0 +1,32 @@
+import { createElement } from 'react';
+
+function Greeting({ name }) {
+    return createElement(
+        'h1',
+        { className: 'greeting' },
+        'Hello ',
+        createElement('i', null, name),
+        '. Welcome!'
+    );
+}
+
+export default function App() {
+    return createElement(
+        Greeting,
+        { name: 'Taylor' }
+    );
+}
+
+/* And here is the same example written using JSX:
+
+function Greeting({ name }) {
+    return (
+        <h1 className="greeting">
+            Hello <i>{name}</i>. Welcome!
+        </h1>
+    );
+}
+
+export default function App() {
+    return <Greeting name="Taylor" />;
+}*/
