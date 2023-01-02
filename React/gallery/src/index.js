5482fad795d3086ef188c3fef911693de689cb43
Joseph Lin <yu.an.lintech@gmail.com>
2023-01-02 20:25:40 +1100

    new and deleted files

---
+++ React/gallery/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+/* The above line of code is telling that the App Component has to be loaded into an html element with id root. 
+This is nothing but the div element present in index.html.*/
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
