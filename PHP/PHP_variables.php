83705956e697cf76977fd2e933b2b091501af8ce
Yuan Lin <yu.an.lintech@gmail.com>
2023-01-11 00:16:48 +1100

    new files

---
+++ PHP/PHP_variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+        $txt = "W3Schools.com";
+        echo "I love $txt!";
+    ?>
+    <!-- The following example will produce the same output as the example above: -->
+    <?php
+        $txt = "W3Schools.com";
+        echo "I love " . $txt . "!";
+    ?>
+    <?php
+        $x = 5;
+        $y = 4;
+        echo $x + $y;
+    ?>
+</body>
+</html>
