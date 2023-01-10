1e7d408c7d268ccc3c8df2dbab8ad8d14878e27c
Yuan Lin <yu.an.lintech@gmail.com>
2023-01-11 00:31:13 +1100

    committed

--- PHP/PHP_echo_print.php
+++ PHP/PHP_echo_print.php
@@ -15,6 +15,11 @@
             echo "<h2>" . $txt1 . "</h2>";
             echo "Study PHP at " . $txt2 . "<br>";
             echo $x + $y;
+
+            print "<h2>PHP is Fun!</h2>";
+            print "Hello world!<br>";
+            print "I'm about to learn PHP!";
+
         ?>  
     </body>
 </html>
