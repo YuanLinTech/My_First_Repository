59744d7568b7316d06b66478cd73a6d65f70a1f6
Yuan Lin <yu.an.lintech@gmail.com>
2023-01-12 20:24:06 +1100

    committed

--- PHP/PHP_database_queries.php
+++ PHP/PHP_database_queries.php
@@ -1,7 +1,8 @@
 <?php
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB; // class DB extends Facade
+use Illuminate\Database\Eloquent\Builder; // class Illuminate\Database\Eloquent\Builder uses the trait Illuminate\Database\Concerns\BuildsQueries
  
-$users = DB::table('users')->get();
+$users = DB::table('users')->get(); // Select the whole "users" table
  
 // table: "users", row: "user", attribute: "name"
 foreach ($users as $user) {
@@ -16,10 +17,10 @@ return $user->email;
 // You can also pass an array of attribute (column) names to the first() method to return the attribute values of the row
 $user = DB::table('users')->where('name', 'John')->first(['email', 'phone']);
 
-// Return the value of the "email" attribute of the row of the table whose "name" attribute equals "John"
+// Return the value of the "email" attribute of the first row of the table whose "name" attribute equals "John"
 $email = DB::table('users')->where('name', 'John')->value('email');
 
-// Return the role in the "users" table whose id value is 3
+// Return the row in the "users" table whose id (primary key) value is 3
 $user = DB::table('users')->find(3);
 
 // Retrieve the "title" column of the table "users"
@@ -33,9 +34,33 @@ foreach ($titles as $title) {
 // Retrieve the "title" column of the table "users" and using the values of the "name" column as keys to create
 // arrays of key-value pairs of name-title
 $titles = DB::table('users')->pluck('title', 'name');
- 
+
 foreach ($titles as $name => $title) {
     echo $title;
 }
 
+// Returns all rows in the "user" table 
+// where the value of the "votes" column is equal to 100 and the value of the age column is greater than 35:
+
+$users = DB::table('users')
+->where('votes', '=', 100)
+->where('age', '>', 35)
+->get();
+
+// Returns all rows in the "user" table where the value of the "votes" column is equal to 100 
+$users = DB::table('users')->where('votes', 100)->get();
+
+$users = DB::table('users')
+                ->where('votes', '>=', 100)
+                ->get();
+
+$users = DB::table('users')
+                ->where('votes', '<>', 100)
+                ->get();
+
+$users = DB::table('users')
+                ->where('name', 'like', 'T%')
+                ->get();
+
+ 
 ?>
