1d79699937b5fbc3fbd1bf5a41f5e26e7c602f93
Yuan Lin <yu.an.lintech@gmail.com>
2023-01-15 16:49:44 +1100

    new and modified files

--- NODE-FIRST-APP/server.js
+++ NODE-FIRST-APP/server.js
@@ -1,25 +1,37 @@
 const http = require('http'); // Importing a global module named HTTP
+const fs = require('fs'); // "fs" stands for "file systems"
+const path = require('path');
+const homePage = fs.readfileSync(path.join(__dirname, 'home.html')); // Find home.html in the current directory
 
 /* The first argument of request listener contains data about the request and the second one will help you send a response. 
 Node will execute the function whenever a request reaches our server. */
 const server = http.createServer((req, res) => {
+    if (req.url === '/about'){
+        res.write('about me');
+        res.end();
+        return;
+    }
+    else if (req.url === '/home')
+    {
+        res.write('Hello');
+        res.end();
+        return;
+    }
+    else
+    {
+        res.write('hello');
+        res.end();
+    }
+    
     console.log(req.url, req.method, req.headers); // Print server request
     // console.log(res); // Print server response
     process.exit(); // Stop listening, unregister the server and quit the process
 }); 
 
-/*
-
-http.createServer((req, res)=>{
-
-}); 
-
-is equivalent to 
-http.createServer(function(req,res){
-
-}); */
 
 /* The listen() method actually starts a process where node.js will not immediately exit our script,
- but where node.js will instead keep this running to listen, that's why the method is named like this for incoming requests.*/
+ but instead keep the script running to listen to a port, that's why the method is named like this for incoming requests.*/
 
-server.listen(3000); // 3000 is the port number
+server.listen(3000); // Let the server listen to port 3000
+/* API server: Firstly, Accept front end request for data. 
+Secondly, retrieve data from the back end. Finally send the data to the front end as the response.*/
