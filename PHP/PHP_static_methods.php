1538012bf9800d8425107ad5394427c310313a99
Yuan Lin <yu.an.lintech@gmail.com>
2023-01-16 22:30:08 +1100

    committed

--- PHP/PHP_static_methods.php
+++ PHP/PHP_static_methods.php
@@ -31,5 +31,26 @@ class domainW3 extends domain {
 $domainW3 = new domainW3;
 echo $domainW3 -> websiteName;
 
+class pi {
+  public static $value=3.14159;
+  public function staticValue() {
+    return self::$value;
+  }
+}
 
+$pi = new pi();
+echo $pi->staticValue(); // 3.14159
+
+class x extends pi {
+  public function xStatic() {
+    return parent::$value;
+  }
+}
+
+// Get value of static property directly via child class
+echo x::$value."<br>"; // 3.14159
+
+// or get value of static property via xStatic() method
+$x = new x();
+echo $x->xStatic(); // 3.14159
 ?>
