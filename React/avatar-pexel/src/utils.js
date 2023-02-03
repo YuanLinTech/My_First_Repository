da49989399cde1f0ffd16b826a8ac71f6ed5a7ab
Yuan Lin <yu.an.lintech@gmail.com>
2023-02-04 10:12:47 +1100

    template literal used

--- React/avatar-pexel/src/utils.js
+++ React/avatar-pexel/src/utils.js
@@ -1,7 +1,5 @@
 export default function getImageUrl(photo, width = '1000', height = '700') {
     return (
-        'https://images.pexels.com/photos/' +
-        photo.imageId + '/pexels-photo-' + 
-        photo.imageId + '.jpeg' + '?w=' + width + '?h=' + height
+        `https://images.pexels.com/photos/${photo.imageId}/pexels-photo-${photo.imageId}.jpeg?w=${width}?h=${height}`
     );
 }
