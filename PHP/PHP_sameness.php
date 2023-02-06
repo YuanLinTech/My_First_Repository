c89c96ef7357a9d2897fceefdad2e5e107fca4e6
Yuan Lin <yu.an.lintech@gmail.com>
2023-02-06 21:46:59 +1100

    committed

---
+++ PHP/PHP_sameness.php
@@ -0,0 +1,14 @@
+<?php
+var_dump(0 == "a");
+var_dump("1" == "01");
+var_dump("10" == "1e1");
+var_dump(100 == "1e2");
+
+// Bool and null are compared as bool always
+var_dump(1 == TRUE);  // TRUE - same as (bool)1 == TRUE
+var_dump(0 == FALSE); // TRUE - same as (bool)0 == FALSE
+var_dump(100 < TRUE); // FALSE - same as (bool)100 < TRUE
+var_dump(-10 < FALSE);// FALSE - same as (bool)-10 < FALSE
+var_dump(min(-100, -10, NULL, 10, 100)); // NULL - (bool)NULL < (bool)-100 is FALSE < TRUE
+
+?>
