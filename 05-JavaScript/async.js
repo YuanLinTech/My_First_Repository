4c44a5c8ea71f620f81e54ace497e5bbd26cae9b
Yuan Lin <yu.an.lintech@gmail.com>
2023-02-08 02:15:39 +1100

    added

--- 05-JavaScript/async.js
+++ 05-JavaScript/async.js
@@ -1,5 +1,5 @@
 console.log("Hello");
-const myTimeout = setTimeout(function(){console.log("Apple")}, 5000);
+const myTimeout = setTimeout(function(){console.log("Apple")}, 5000); // Did not execute because timeout cleared after 3s
 console.log("JavaScript rules");
 
 function myStopFunction() {
