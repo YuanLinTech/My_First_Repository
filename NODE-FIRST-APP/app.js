4ab57f16b6454250b35e850844ada76f658b3a87
Yuan Lin <yu.an.lintech@gmail.com>
2023-02-11 00:50:57 +1100

    committed

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -1,3 +1,8 @@
-const{increment, getCurrentCount} = require('./index.js');
-increment();
-console.log(getCurrentCount()); // 1
+const http = require("http");
+const server = http.createServer(function(req,res){
+    res.setHeader('Content-Type', 'text/html'); // Setting a single header value for implicit headers. Returns the response object.
+    res.writeHead(200, {"Content-Type":"text/plain"}); // Sending a response header to the request. Response code 200 indicates the request succeeded.
+    res.write("This is a really cool message.");// To send a response message back to the client making the request
+    res.end();// Signalling to the server that all response headers and body have been sent
+});
+server.listen(3000, "localhost"); // (port number, host name)
