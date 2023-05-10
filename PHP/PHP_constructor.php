900d8e948a8537266052c6e4e40ba29852bfa490
Yuan Lin <yu.an.lintech@gmail.com>
2023-05-11 00:40:34 +1000

    new file

---
+++ PHP/PHP_constructor.php
@@ -0,0 +1,27 @@
+<?php
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function get_color() {
+    return $this->color;
+  }
+  function __destruct() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
+}
+
+$apple = new Fruit("Apple", "red");
+echo $apple->get_name(); // Apple
+echo "<br>";
+echo $apple->get_color(); // red
+echo "<br>";
+// "The fruit is Apple and the color is red."
+?>
