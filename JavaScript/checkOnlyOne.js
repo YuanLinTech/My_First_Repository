6b0104a919e2882a4f080387ed4d6bca8c11725a
Yuan Lin <yu.an.lintech@gmail.com>
2023-07-25 22:05:45 +1000

    Deleted node_modules folder

--- JavaScript/checkOnlyOne.js
+++ JavaScript/checkOnlyOne.js
@@ -1,12 +1,8 @@
-//JavaScript function to allow checking of one "Hobby" checkbox at the same time:
+//JavaScript function to allow checking of a group of checkbox with the "name" value of "check" at the same time:
 
-function checkOnlyOne(b) {
-    var x = document.getElementsByClassName("hobbies");
-    var i;
-
-    for (i = 0; i < x.length; i++) {
-        if (x[i].value != b) {
-            x[i].checked = false;
-        }
-    }
+function checkOnlyOne(checkbox) {
+    var checkboxes = document.getElementsByName('check');
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false;
+    })
 }
