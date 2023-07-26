2069406dbb8b1492b6837a9f7e3383921b56ad91
Yuan Lin <yu.an.lintech@gmail.com>
2023-07-27 09:56:48 +1000

    created, renamed and deleted files

--- TypeScript/TypeScript_in_5_min.ts
+++ TypeScript/TypeScript_in_5_min.ts
@@ -10,14 +10,13 @@ interface Backpack<Type> {
     get: () => Type;
 }
 
-// This line is a shortcut to tell TypeScript there is a
-// constant called `backpack`, and to not worry about where it came from.
+// This line is a shortcut to tell TypeScript there is a constant called `backpack`, and to not worry about where it came from.
 declare const backpack: Backpack<string>;
 
 // object is a string, because we declared it above as the variable part of Backpack.
 const object = backpack.get();
 
-// Since the backpack variable is a string, you can't pass a number to the add function.
+// Since the backpack variable is of type Backpack<string>, you can't pass a number to the add function.
 // backpack.add(23); Argument of type 'number' is not assignable to parameter of type 'string'.
 backpack.add("My bag");
 
