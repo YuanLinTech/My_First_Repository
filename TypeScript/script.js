26d67a2663783b205dabf33f0e2f6af19b7a3a65
Yuan Lin <yu.an.lintech@gmail.com>
2023-07-26 22:34:48 +1000

    committed

---
+++ TypeScript/script.js
@@ -0,0 +1,10 @@
+/* Object types can be anonymous */
+function greet(person) {
+    return "Hello " + person.name;
+}
+function greeting(person) {
+    return "Hello " + person.name;
+}
+function greetings(person) {
+    return "Hello " + person.name;
+}
--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,7 +1,26 @@
-function sayHelloWorld(): void {
-    console.log("\n");
-    console.log("*** Hello World from Type Script code. ***");
-    console.log("\n");
+/* Object types can be anonymous */
+
+function greet(person: { name: string; age: number }) {
+    return "Hello " + person.name;
 }
-  
-sayHelloWorld();
+
+/* or they can be named by using either an interface: */
+
+interface Person{
+    name: string;
+    age: number;
+}
+   
+function greeting(person: Person) {
+    return "Hello " + person.name;
+}
+
+/* or a type alias: */
+type TypePerson = {
+    name: string;
+    age: number;
+};
+   
+function greetings(person: TypePerson) {
+    return "Hello " + person.name;
+}
