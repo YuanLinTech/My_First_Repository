a10ecb00e9af835216b09abae440707fabee02cd
Yuan Lin <yu.an.lintech@gmail.com>
2023-08-12 00:00:37 +1000

    committed

--- JavaScript/JavaScript/Destructuring_assignments.js
+++ JavaScript/JavaScript/Destructuring_assignments.js
@@ -3,6 +3,7 @@ var [y, z, ...rest] = x;
 console.log(y); // y = 1
 console.log(z); // z = 2
 console.log(rest); // rest = [3, 4, 5]
+console.log('\n');
 
 const obj = { A: 1, B: { C: 2 } };
 const { A, B: { C: D } } = obj;
@@ -12,6 +13,12 @@ const { A, B: { C: D } } = obj;
 // const {C: D} = obj.B;
 console.log(A); // 1
 console.log(D); // 2
+console.log('\n');
+
+var myObj = { a: 1, b: 2 };
+var { a } = myObj; // a is constant
+// const { a } = obj is equivalent to const a = myObj.a;
+console.log(a); // 1
 
 var myObj = { a: 1, b: { c: 2 } };
 var { a } = myObj; // a is constant
@@ -21,17 +28,19 @@ console.log(a); // 1
 let { b: { c: d } } = myObj; // d is re-assignable
 // let { b: { c: d } } = obj is equivalent to {c: d} = obj.b, which is equivalent to {c: d} = {c: 2}
 console.log(d); // 2
-
+console.log('\n');
 
 var x = ([a, b] = [10, 20]) => a + b; /* Used destructuring assignment to assign a the default value of 10 and b the default value of 20. */
 console.log(x()); // Output: 30
 // console.log(x(2, 3)); // TypeError: number 2 is not iterable (cannot read property Symbol(Symbol.iterator))
 console.log(x([2, 3])); // Output: 5
+console.log('\n');
 
 var y = ({ a, b } = { a: 10, b: 20 }) => a + b; /* Used destructuring assignment to assign a the default value of 10 and b the default value of 20. */
 console.log(y()); // Output: 30
 // console.log(y({2,3})); SyntaxError: Unexpected token ','
 console.log(y({a: 2, b: 3})); // Output: 5
+console.log('\n');
 
 const numbers1 = [];
 const obj1 = { a: 1, b: 2 };
@@ -41,6 +50,7 @@ const obj1 = { a: 1, b: 2 };
 // numbers1[0] = obj1.a;
 // numbers1[1] = obj1.b;
 console.log(numbers1); // Output: [ 1, 2 ]
+console.log('\n');
 
 /*
 Note that the equivalent binding pattern of the code above is not valid syntax:
@@ -65,6 +75,7 @@ var obj2 = { a: 1, b: 2 };
 // numbers2[0] = obj2.a;
 // numbers2[1] = obj2.b;
 console.log(numbers2); // Output: [ 1, 2 ]
+console.log('\n');
 
 /* var { a: numbers2[0], b: numbers2[1] } = obj2; // SyntaxError: Invalid destructuring assignment target
 // This is equivalent to:
@@ -75,15 +86,18 @@ console.log(numbers2); // Output: [ 1, 2 ]
 var { a, ...others } = { a: 1, b: 2, c: 3 };
 console.log(a); // Output: 1
 console.log(others); // Output: { b: 2, c: 3 }
+console.log('\n');
 
 const [first, ...others2] = [1, 2, 3];
 console.log(others2); // Output: [2, 3]
+console.log('\n');
 
 var foo = ['one', 'two', 'three'];
 var [red, yellow, green] = foo;
 console.log(red); // Output: "one"
 console.log(yellow); // Output: "two"
 console.log(green); // Output: "three"
+console.log('\n');
 
 var foo = ['one', 'two'];
 var [red, yellow, green, blue] = foo;
@@ -91,6 +105,7 @@ console.log(red); // Output: "one"
 console.log(yellow); // Output: "two"
 console.log(green); // Output: undefined
 console.log(blue);  // Output: undefined
+console.log('\n');
 
 // Swapping variables
 let m = 1;
@@ -99,10 +114,12 @@ let n = 3;
 [m, n] = [n, m];
 console.log(m); // Output: 3
 console.log(n); // Output: 1
+console.log('\n');
 
 const arr = [1, 2, 3];
 [arr[2], arr[1]] = [arr[1], arr[2]];
 console.log(arr); // Output: [1, 3, 2]
+console.log('\n');
 
 function f() {
     return [1, 2, 3];
@@ -111,9 +128,11 @@ function f() {
 [a, , b] = f();
 console.log(a); // Output: 1
 console.log(b); // Output: 3
+console.log('\n');
 
 [c] = f();
 console.log(c); // Output: 1
+console.log('\n');
 
 var o = { p: 42, q: true };
 var { p: foo, q: bar } = o;
